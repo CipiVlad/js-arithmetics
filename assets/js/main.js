6b2eb7f0f117102125de545320e2785940ea20b7
CipiVlad <supercoder.cipi@gmail.com>
2022-04-13 23:58:39 +0200

    js-arithmetics

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+// let score = 5 + 5 * 10;
+// console.log("Score: " + score);
+
+// let score = (5 + 5) * 10;
+// console.log('Ergebnis: ' + score);
+
+// let score = 0;
+// score = score + 10;
+// console.log("Score: " + score);
+
+// Hiervon gibt es eine verkürzte Version.
+// let score = 0;
+// score += 10;
+// console.log("Score: " + score);
+
+// let zahl = 1;
+// zahl = zahl + 1;
+// zahl += 1;
+// // zahl++;
+// // console.log("increment: " + zahl);
+// zahl--;
+// console.log('dekrement: ' + zahl);
+
+
